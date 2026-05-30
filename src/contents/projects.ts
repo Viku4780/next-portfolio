503ef1491b765d2efb7baf5108c3fb208ecab08d
Vikrant <vikrantmom5748@gmail.com>
2026-05-30 13:20:08 +0530

    changed Portfolio project image

--- src/contents/projects.ts
+++ src/contents/projects.ts
@@ -10,21 +10,14 @@ export const projects: Project[] = [
         demoLink: 'https://multivendor-cart.vercel.app/',
         image: '/projects/e-commerce.png',
       },
-      // {
-      //   title: 'Task Management App',
-      //   description: 'A collaborative task management application with real-time updates.',
-      //   technologies: ['React', 'Node.js', 'MongoDB', 'Socket.io'],
-      //   githubLink: 'https://github.com',
-      //   demoLink: 'https://demo.com',
-      //   image: '/projects/task-manager.webp',
-      // },
+     
       {
         title: 'Portfolio Website',
         description: 'My personal portfolio website showcasing my projects and skills.',
         technologies: ['Next.js', 'Tailwind CSS', 'TypeScript','Framer Motion'],
         githubLink: 'https://github.com/Viku4780/next-portfolio',
         demoLink: 'https://next-portfolio-lac-gamma-96.vercel.app/',
-        image: '/projects/portfolio-app.png',
+        image: '/projects/portfolio-web.png',
       },
       {
         title: 'Blog Platform',
@@ -34,14 +27,7 @@ export const projects: Project[] = [
         demoLink: 'https://blog-app-x18y.onrender.com/',
         image: '/projects/blog-app.png',
       },
-      // {
-      //   title: 'Weather App',
-      //   description: 'A weather application that provides real-time weather updates.',
-      //   technologies: ['React', 'OpenWeatherMap API'],
-      //   githubLink: 'https://github.com',
-      //   demoLink: 'https://demo.com',
-      //   image: '/projects/weather-app.png',
-      // },
+      
       {
         title: 'Chat Application',
         description: 'A real-time chat application using WebSocket technology.',
@@ -50,20 +36,5 @@ export const projects: Project[] = [
         demoLink: 'https://real-time-chat-app-83dt.onrender.com/',
         image: '/projects/chat-application.png',
       },
-      // {
-      //   title: 'Recipe Finder',
-      //   description: 'A recipe search application using the Edamam API.',
-      //   technologies: ['React', 'Edamam API'],
-      //   githubLink: 'https://github.com',
-      //   demoLink: 'https://demo.com',
-      //   image: '/projects/recipe-finder.png',
-      // },
-      // {
-      //   title: 'Expense Tracker',
-      //   description: 'A personal finance tracker to manage expenses and income.',
-      //   technologies: ['React', 'Firebase'],
-      //   githubLink: 'https://github.com',
-      //   demoLink: 'https://demo.com',
-      //   image: '/projects/expense-tracker.webp',
-      // }
+   
   ];
